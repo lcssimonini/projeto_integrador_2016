ae0bafa6930a881c085cccc6a49972fa1f9094e9
lcssimonini <lcs.simonini@gmail.com>
2016-06-21 22:19:49 -0300

    novo site

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/cursos/<course_id>')
+def show_course_page(course_id):
+    return render_template('courses.html', course_id=course_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
