438c0d5eef01c4ff504ddda2f1b6dc596dcbbd81
lcssimonini <lcs.simonini@gmail.com>
2016-06-24 12:45:43 -0300

    paginas de cursos

--- app.py
+++ app.py
@@ -3,11 +3,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", menu_cursos=False)
 
-@app.route('/cursos/<course_id>')
-def show_course_page(course_id):
-    return render_template('courses.html', course_id=course_id)
+@app.route("/arquitetura")
+def arquitetura():
+    return render_template("arquitetura.html", menu_cursos=True)
+
+@app.route("/webdesign")
+def webdesign():
+    return render_template("webdesign.html", menu_cursos=True)
+
+@app.route("/logica")
+def logica():
+    return render_template("logica.html", menu_cursos=True)
+
+@app.route("/ingles")
+def ingles():
+    return render_template("ingles.html", menu_cursos=True)
+
+@app.route("/metodologia")
+def metodologia():
+    return render_template("metodologia.html", menu_cursos=True)
+
+@app.route("/sistemasop")
+def sistemasop():
+    return render_template("sistemasop.html", menu_cursos=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
